39d335b6647a6cdf9b03083c2cfab0fddb665c4e
tomasjakub <tomasjakub21@gmail.com>
2021-10-07 19:02:18 +0200

    register form for business/company

--- src/app/pages/business/register-business/register-business.page.ts
+++ src/app/pages/business/register-business/register-business.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register-business',
@@ -6,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-business.page.scss'],
 })
 export class RegisterBusinessPage implements OnInit {
+
+  registerForm: FormGroup;
+
+  successMsg: string = '';
+  errorMsg: string = '';
+  firebaseErrorMessage: string;
+
+
+  error_msg = {
+    'email': [
+      {
+        type: 'required',
+        message: 'Provide email.'
+      },
+      {
+        type: 'pattern',
+        message: 'Email is not valid.'
+      }
+    ],
+    'password': [
+      {
+        type: 'required',
+        message: 'Password is required.'
+      },
+      {
+        type: 'minlength',
+        message: 'Password length should be 6 characters long.'
+      }
+    ],
+    'password2': [
+      {
+        type: 'required',
+        message: 'Password is required.'
+      },
+
+    ]
+  };
+
+
+
 
   constructor() { }
 
