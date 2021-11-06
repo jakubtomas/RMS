783c79b8b8cd27fc7f4034825c2da16a41669e91
tomasjakub <tomasjakub21@gmail.com>
2021-11-06 14:52:51 +0100

    add checkboxies

--- src/app/pages/business/create-calendar/create-calendar.page.ts
+++ src/app/pages/business/create-calendar/create-calendar.page.ts
@@ -1,44 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-create-calendar',
-  templateUrl: './create-calendar.page.html',
-  styleUrls: ['./create-calendar.page.scss'],
+    selector: 'app-create-calendar',
+    templateUrl: './create-calendar.page.html',
+    styleUrls: ['./create-calendar.page.scss'],
 })
 export class CreateCalendarPage implements OnInit {
 
-  messageFirebase: string;
-  registerForm: FormGroup;
+    messageFirebase: string;
+    registerForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
-
-  get nameCalendar(): FormControl {
-    return this.registerForm.get('nameCalendar') as FormControl;
-  }
-  get openingHours(): FormControl {
-    return this.registerForm.get('openingHours') as FormControl;
-  }
-
-
-  ngOnInit() {
-    this.messageFirebase = null;
-
-    if (this.route.snapshot.paramMap.get('createdBusiness')) {
-      this.messageFirebase = 'Business successfully created'
+    constructor(private route: ActivatedRoute,
+    ) {
+        this.dessertOptions.forEach(() => {
+            this.ordersFormArray.push(new FormControl(false))
+        })
     }
 
-    this.registerForm = new FormGroup(
-        {
-          nameCalendar: new FormControl('', Validators.required),
-          openingHours: new FormControl('', Validators.required),
+    get nameCalendar(): FormControl {
+        return this.registerForm.get('nameCalendar') as FormControl;
+    }
 
+    get openingHours(): FormControl {
+        return this.registerForm.get('openingHours') as FormControl;
+    }
+
+    ngOnInit() {
+        this.messageFirebase = null;
+
+        if (this.route.snapshot.paramMap.get('createdBusiness')) {
+            this.messageFirebase = 'Business successfully created'
         }
-    );
 
+        this.registerForm = new FormGroup(
+            {
+                nameCalendar: new FormControl('', Validators.required),
+                openingHours: new FormControl('', Validators.required),
 
+            }
+        );
+    }
 
-  }
+    /*////////////////////////////////////////////////////////*/
+    contactForm = new FormGroup({
+        firstName: new FormControl("", Validators.required),
+        lastName: new FormControl("", Validators.required),
+        options: new FormArray([]),
+    });
+    dessertOptions: string[] = ["Monday", "Tuesday", "Wednesday",
+                                "Thursday", "Friday", "Saturday", "Sunday"];
+
+    get ordersFormArray() {
+        return this.contactForm.controls.options as FormArray;
+    }
+
+    submitForm() {
+        console.log(this.contactForm.value);
+    }
+
 
 }
